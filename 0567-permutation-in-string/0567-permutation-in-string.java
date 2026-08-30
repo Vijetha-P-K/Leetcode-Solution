c3173d0bbf4e2c2155e15f72ddd7d1775ec47241
Vijetha  P K <vijethakanagaraj@gmail.com>
2026-08-30 21:16:46 +0530

    Time: 9 ms (38.7%), Space: 43.9 MB (85.21%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+class Solution { 
+    public boolean checkInclusion(String s1, String s2) { 
+        if (s1.length() > s2.length()) { 
+            return false; 
+        } 
+        int[] s1Count = new int[26]; 
+        int[] s2Count = new int[26]; 
+        
+        for (int i = 0; i < s1.length(); i++) { 
+            s1Count[s1.charAt(i) - 'a']++; 
+            s2Count[s2.charAt(i) - 'a']++; 
+        }
+        for (int j = s1.length(); j < s2.length(); j++) { 
+            if (Arrays.equals(s1Count, s2Count)) { 
+                return true; 
+            } 
+            s2Count[s2.charAt(j) - 'a']++; 
+            s2Count[s2.charAt(j - s1.length()) - 'a']--; 
+        } 
+        if (Arrays.equals(s1Count, s2Count)) { 
+            return true; 
+        } 
+        return false; 
+    } 
+}

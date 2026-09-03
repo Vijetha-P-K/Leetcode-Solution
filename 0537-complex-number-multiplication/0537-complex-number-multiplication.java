08ab6feddc33889e4b9db2f5a0fd1720ed158b26
Vijetha  P K <vijethakanagaraj@gmail.com>
2026-09-03 16:35:34 +0530

    Time: 3 ms (59.32%), Space: 43.1 MB (14.02%) - LeetHub

---
+++ 0537-complex-number-multiplication/0537-complex-number-multiplication.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String complexNumberMultiply(String num1, String num2) {
+        String[] parts1 = num1.split("\\+");
+        int a = Integer.parseInt(parts1[0]);
+        int b = Integer.parseInt(parts1[1].replace("i", ""));
+        String[] parts2 = num2.split("\\+");
+        int c = Integer.parseInt(parts2[0]);
+        int d = Integer.parseInt(parts2[1].replace("i", ""));
+        int real = a * c - b * d;
+        int imaginary = a * d + b * c;
+
+        return real + "+" + imaginary + "i";
+    }
+}
